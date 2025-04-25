771eb4dfaf43cab669ce325ef3109c80b0739c14
github-action-benchmark <github@users.noreply.github.com>
2025-04-25 15:48:26 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for b77be57eb982291a32c2bb3c02559933e18e541a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745531300739,
+  "lastUpdate": 1745596105830,
   "repoUrl": "https://github.com/fondoger/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -76,6 +76,44 @@ window.BENCHMARK_DATA = {
             "range": "±0.65%",
             "unit": "ops/sec",
             "extra": "236 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "thomas.norling@microsoft.com",
+            "name": "Thomas Norling",
+            "username": "tnorling"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b77be57eb982291a32c2bb3c02559933e18e541a",
+          "message": "Update dependencies (#7718)\n\nUpdates several dependencies to resolve CVEs",
+          "timestamp": "2025-04-25T08:49:50-04:00",
+          "tree_id": "aaaaa9c4aea1f274ae77edd1439491eba0fe5be4",
+          "url": "https://github.com/fondoger/microsoft-authentication-library-for-js/commit/b77be57eb982291a32c2bb3c02559933e18e541a"
+        },
+        "date": 1745596103468,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 255372,
+            "range": "±0.98%",
+            "unit": "ops/sec",
+            "extra": "231 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 254734,
+            "range": "±0.91%",
+            "unit": "ops/sec",
+            "extra": "220 samples"
           }
         ]
       }
