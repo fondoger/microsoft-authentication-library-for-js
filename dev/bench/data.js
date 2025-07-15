7560fcf84dde6878e0d109bf56144a243a31877b
github-action-benchmark <github@users.noreply.github.com>
2025-07-15 03:48:29 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for 2d931672bcdd0e18ac6b780971d8029ef7cb7872

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749095317150,
+  "lastUpdate": 1752551309449,
   "repoUrl": "https://github.com/fondoger/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -570,6 +570,44 @@ window.BENCHMARK_DATA = {
             "range": "±0.68%",
             "unit": "ops/sec",
             "extra": "235 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dasau@microsoft.com",
+            "name": "Dan Saunders",
+            "username": "codexeon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2d931672bcdd0e18ac6b780971d8029ef7cb7872",
+          "message": "Fix exception when using claims with Nested App Auth in JS Runtime environment (#7926)\n\nJS Runtime does not support full crypto API, and is running into an\nexception in hydrateCache while trying to generate a sha-256 hash of the\nclaims. The token response is received, but never returned to developer\nbecause writing to cache failed. For this scenario, there is already a\ncache in the host, so it is still able to avoid a network call if a\nprevious request with the same claim was made.",
+          "timestamp": "2025-07-14T12:35:08-07:00",
+          "tree_id": "545e3b391595e3034c2474db680a56747422da46",
+          "url": "https://github.com/fondoger/microsoft-authentication-library-for-js/commit/2d931672bcdd0e18ac6b780971d8029ef7cb7872"
+        },
+        "date": 1752551307648,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 249742,
+            "range": "±0.89%",
+            "unit": "ops/sec",
+            "extra": "237 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 255266,
+            "range": "±0.57%",
+            "unit": "ops/sec",
+            "extra": "236 samples"
           }
         ]
       }
