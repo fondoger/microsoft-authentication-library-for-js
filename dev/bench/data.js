387be35da921e611df78c1f5b814178ec8499325
github-action-benchmark <github@users.noreply.github.com>
2025-08-21 21:48:22 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for 190730d86116ba71bc407c194cc7c9d1fd1c94ef

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755661706769,
+  "lastUpdate": 1755812901748,
   "repoUrl": "https://github.com/fondoger/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -1178,6 +1178,44 @@ window.BENCHMARK_DATA = {
             "range": "±0.87%",
             "unit": "ops/sec",
             "extra": "237 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "thomas.norling@microsoft.com",
+            "name": "Thomas Norling",
+            "username": "tnorling"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "190730d86116ba71bc407c194cc7c9d1fd1c94ef",
+          "message": "Fix CODEOWNERS for custom-auth (#8011)\n\nFixes the Codeowners path for the custom-auth features",
+          "timestamp": "2025-08-21T18:07:22Z",
+          "tree_id": "5ff7125c274f36009d045ae2db7aea061d828e99",
+          "url": "https://github.com/fondoger/microsoft-authentication-library-for-js/commit/190730d86116ba71bc407c194cc7c9d1fd1c94ef"
+        },
+        "date": 1755812899526,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 248751,
+            "range": "±0.97%",
+            "unit": "ops/sec",
+            "extra": "221 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 247489,
+            "range": "±0.77%",
+            "unit": "ops/sec",
+            "extra": "223 samples"
           }
         ]
       }
