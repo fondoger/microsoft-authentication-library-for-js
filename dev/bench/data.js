70e1f23af613e1499e0222f9d58896094d2a3305
github-action-benchmark <github@users.noreply.github.com>
2025-10-08 15:48:28 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for ffe81abe542469e37266bf69a7837323541a8d54

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759787300363,
+  "lastUpdate": 1759938508372,
   "repoUrl": "https://github.com/fondoger/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -2165,6 +2165,44 @@ window.BENCHMARK_DATA = {
             "range": "±1.06%",
             "unit": "ops/sec",
             "extra": "236 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "45603194+shenj@users.noreply.github.com",
+            "name": "Jian Shen",
+            "username": "shenj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ffe81abe542469e37266bf69a7837323541a8d54",
+          "message": "[Native Auth] Update the logic for detecting phone blocked errors (#8087)",
+          "timestamp": "2025-10-08T14:16:02+01:00",
+          "tree_id": "a20929ace6fed5e8c0586e7b9b5686eae2a78aa4",
+          "url": "https://github.com/fondoger/microsoft-authentication-library-for-js/commit/ffe81abe542469e37266bf69a7837323541a8d54"
+        },
+        "date": 1759938506149,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 260456,
+            "range": "±0.72%",
+            "unit": "ops/sec",
+            "extra": "236 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 254782,
+            "range": "±0.78%",
+            "unit": "ops/sec",
+            "extra": "224 samples"
           }
         ]
       }
