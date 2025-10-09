37b5469e12dc8d02bb86fdb72a4a567baf5a7b34
github-action-benchmark <github@users.noreply.github.com>
2025-10-09 16:03:25 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for 1e6420e364bc800c3299e9c6593a1b2e750a9852

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759938508372,
+  "lastUpdate": 1760025805515,
   "repoUrl": "https://github.com/fondoger/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -2203,6 +2203,44 @@ window.BENCHMARK_DATA = {
             "range": "±0.78%",
             "unit": "ops/sec",
             "extra": "224 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "45603194+shenj@users.noreply.github.com",
+            "name": "Jian Shen",
+            "username": "shenj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1e6420e364bc800c3299e9c6593a1b2e750a9852",
+          "message": "[Native Auth] Introduce the stateType property in state classes for the type detection (#8090)\n\nIntroduce a new property stateType in all state classes for avoid the\ncircular dependency issue which may happen in some scenarios.",
+          "timestamp": "2025-10-09T16:27:59+01:00",
+          "tree_id": "1e03b182442c03cd88c0339a33d96714382bfb82",
+          "url": "https://github.com/fondoger/microsoft-authentication-library-for-js/commit/1e6420e364bc800c3299e9c6593a1b2e750a9852"
+        },
+        "date": 1760025803898,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 249961,
+            "range": "±0.71%",
+            "unit": "ops/sec",
+            "extra": "235 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 248902,
+            "range": "±0.74%",
+            "unit": "ops/sec",
+            "extra": "212 samples"
           }
         ]
       }
