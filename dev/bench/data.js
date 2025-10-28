9c99408f4f788b459e3c01132e5a7050f3b24d3a
github-action-benchmark <github@users.noreply.github.com>
2025-10-28 21:48:23 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for 5a25c350f0e7a18e1d88ae6393fd6e847595da83

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761342509047,
+  "lastUpdate": 1761688103128,
   "repoUrl": "https://github.com/fondoger/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -2393,6 +2393,44 @@ window.BENCHMARK_DATA = {
             "range": "±0.59%",
             "unit": "ops/sec",
             "extra": "224 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "137432604+Ugonnaak1@users.noreply.github.com",
+            "name": "Ugonna Akali",
+            "username": "Ugonnaak1"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5a25c350f0e7a18e1d88ae6393fd6e847595da83",
+          "message": "update msal-node-runtime version to 0.20.0 (#8113)",
+          "timestamp": "2025-10-28T09:05:56-07:00",
+          "tree_id": "bd49a8c23646285c082c5a5852613e9696f45b22",
+          "url": "https://github.com/fondoger/microsoft-authentication-library-for-js/commit/5a25c350f0e7a18e1d88ae6393fd6e847595da83"
+        },
+        "date": 1761688101538,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 248829,
+            "range": "±0.89%",
+            "unit": "ops/sec",
+            "extra": "224 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 250100,
+            "range": "±0.81%",
+            "unit": "ops/sec",
+            "extra": "235 samples"
           }
         ]
       }
