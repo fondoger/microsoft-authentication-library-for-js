8b04d619b66cfcd8478d1c7c07d82efeaa69da8b
github-action-benchmark <github@users.noreply.github.com>
2025-11-13 21:48:32 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for 3c83743c458d8c49f7421fb232eca77ea4a2a7d2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763005700235,
+  "lastUpdate": 1763070512328,
   "repoUrl": "https://github.com/fondoger/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -2583,6 +2583,44 @@ window.BENCHMARK_DATA = {
             "range": "±0.83%",
             "unit": "ops/sec",
             "extra": "222 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "137432604+Ugonnaak1@users.noreply.github.com",
+            "name": "Ugonna Akali",
+            "username": "Ugonnaak1"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3c83743c458d8c49f7421fb232eca77ea4a2a7d2",
+          "message": "Handle MsalRuntime Error Surfacing  (#8120)\n\nAimed to solve the problem discussed\n[here:](https://identitydivision.visualstudio.com/DevEx/_git/AuthLibrariesApiReview/pullrequest/20409?path=/MSALJS/%5Bmsal-node%5D%20MsalRuntime/Surface_MsalRuntime_Errors.md)\nSolution 5 implementation",
+          "timestamp": "2025-11-13T13:24:18-08:00",
+          "tree_id": "32c0065af8970f35a8f83dc6acd2954b1201d484",
+          "url": "https://github.com/fondoger/microsoft-authentication-library-for-js/commit/3c83743c458d8c49f7421fb232eca77ea4a2a7d2"
+        },
+        "date": 1763070510164,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 243666,
+            "range": "±0.81%",
+            "unit": "ops/sec",
+            "extra": "233 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 244497,
+            "range": "±0.73%",
+            "unit": "ops/sec",
+            "extra": "234 samples"
           }
         ]
       }
