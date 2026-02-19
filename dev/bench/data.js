3c3f8292c3bc5f84449f57aeceb13b40badff029
github-action-benchmark <github@users.noreply.github.com>
2026-02-19 21:48:49 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for 6b01484da1062075af3ee8a60433438b71904cc5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771019315358,
+  "lastUpdate": 1771537729680,
   "repoUrl": "https://github.com/fondoger/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -3341,6 +3341,44 @@ window.BENCHMARK_DATA = {
             "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
             "value": 241196,
             "range": "±0.81%",
+            "unit": "ops/sec",
+            "extra": "224 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kshabelko@microsoft.com",
+            "name": "Konstantin",
+            "username": "konstantin-msft"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6b01484da1062075af3ee8a60433438b71904cc5",
+          "message": "[v4] Add silentRefreshReason telemetry field to PerformanceEvent (#8343)\n\nAdd silentRefreshReason field to telemetry and update related tests",
+          "timestamp": "2026-02-19T12:53:14-08:00",
+          "tree_id": "4020702d32e37f33ef3dd1d728a478df10dd2024",
+          "url": "https://github.com/fondoger/microsoft-authentication-library-for-js/commit/6b01484da1062075af3ee8a60433438b71904cc5"
+        },
+        "date": 1771537727512,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 249767,
+            "range": "±0.84%",
+            "unit": "ops/sec",
+            "extra": "235 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 246690,
+            "range": "±0.77%",
             "unit": "ops/sec",
             "extra": "224 samples"
           }
